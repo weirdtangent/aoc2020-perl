d7d89fdfa3a59bb88b9c4d25ecc2122f0e46e1bc
Jeff Culverhouse <jeff@graystorm.com>
2020-12-08 16:55:39 +0000

    AoC 2020 #8 unnecessary fixes, extra checking, extra logging

--- advent08/advent08b.pl
+++ advent08/advent08b.pl
@@ -7,17 +7,24 @@ while (my $line = <STDIN>) {
   push @code, $line;
 }
 
+# try as-is
+exit if trychange(-1, @code);
+
 while (1) {
-  trychange($change, @code);
+  die "Failed to fix!" if $change > scalar(@code);
+  exit if trychange($change, @code);
   $change+=1;
 }
 
 sub trychange {
   my ($change, @code) = @_;
 
-  print "\nTry changing opcode #$change\n";
+  $change < 0
+    ? print "\nTry running as-is\n"
+    : print "\nTry changing opcode #$change\n";
 
   my $linenum=0;
+  my $changed_linenum=0;
   my $acc=0;
   my %track;
   my $opcount=0;
@@ -25,7 +32,7 @@ sub trychange {
 
   while ($linenum < $maxlines) {
     if (exists $track{$linenum}) {
-      print "infinite loop detected, aborting\n";
+      print "infinite loop detected, about to run ".($linenum+1)." again, aborting with acc = $acc\n";
       return 0;
     }
 
@@ -34,9 +41,10 @@ sub trychange {
     my $line = $code[$linenum];
     my ($opcode, $sign, $value) = $line =~ /(\w+) ([-+])(\d+)/;
 
-    if ($opcount == $change) {
+    if ($change >= 0 && $opcount == $change) {
       if ($opcode eq 'nop')    { print "Changing nop to jmp\n"; $opcode='jmp' }
       elsif ($opcode eq 'jmp') { print "Changing jmp to nop\n"; $opcode='nop' }
+      $changed_linenum = $linenum+1;
     }
 
     if ($opcode eq 'nop') {
@@ -50,8 +58,12 @@ sub trychange {
       $linenum += ($sign eq '+') ? $value : ($value * -1);
     }
 
-    $opcount++;
+    $opcount++ if $opcode =~ /nop|jmp/;
   }
 
-  die "Hit end of code, acc = $acc\n";
+  $change < 0
+    ? print "Hit end of code by running it as-is, acc = $acc\n"
+    : print "Hit end of code by fixing line number $changed_linenum, acc = $acc\n";
+
+  return 1;
 }
